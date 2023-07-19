e84b548dcb0e781d75876a3a13ed3096dde33e5f
JoseSojo <jsojo346@gmail.com>
2023-07-19 11:10:43 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,34 @@ export default function App() {
   const [notices, setNotice] = useState([]);
   const [country, setCountry] = useState('ve');
 
-  // 'https://newsapi.org/v2/everything?q=bitcoin&apiKey='
-  const url = 'https://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=' + apiKey
+  const url = 'https://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=eabfc79a266949ed8a7f11b2f3248677';
 
   useEffect(()=> {
     fetch(url)
-    .then(res => res.json())
+    .then(res => {
+      res.json();
+      console.log(res);
+    })
     .then(data => {
-      setNotice(data.articles);
-
-      for (let i = 0; i < notices.length; i++) {
-        console.log(notices[i]);
-        break;
-      }
-    });
+      console.log(data);
+      //setNotice(data.articles)
+      //console.log(data);
+    })
   }, [country]);
 
   const handleClickCountry = e => setCountry(e.target.innerText);
 
   return (
     <>
-
       <div className='grid bg-gray-300'>
         <h2 className='font-extrabold text-3xl py-3'>Cambiar Pais</h2>
         <p>Pais Actual: <span className='font-bold text-emerald-500'>{country}</span></p>
-        
+
         <section className='container'>
           {
-            allCountrys.map(key =>
+            allCountrys.map((key,index) =>
               <Badge
+                key={index}
                 text={key}
                 UpdateCountry={handleClickCountry}
                 />
@@ -48,7 +47,7 @@ export default function App() {
       <main className='grid grid-cols-1 md:grid-cols-2 grid-gap-4 p-4 bg-gray-300'>
         {
           notices.map(key =>
-            <Notice 
+            <Notice
               urlToImage={key.urlToImage}
               author={key.uthor}
               title={key.title}
--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,6 +4,6 @@ export default function Badge({text, UpdateCountry}) {
 			onClick={UpdateCountry}
 			className='inline-block rounded-full text-white bg-blue-500 hover:bg-gray-500 duration-300 text-xs font-bold mr-1 md:mr-2 mb-2 px-2 md-px-2 md:px-4 py-1 opacity-90 hover:opacity-100'>
 				{text}
-	    </aside>
+    </aside>
 	);
 }
